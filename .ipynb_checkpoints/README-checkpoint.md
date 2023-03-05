999023c49a8469fcbf67456364764e7820ae6e37
paulbqnt <paul.boquant@gmail.com>
2023-03-04 20:56:09 -0500

    add identification, portfolio methods

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,2 +0,0 @@
-# hiram
-Hiram is a free financial library built in  python that can be used for option pricing
--- bond.py
+++ bond.py
@@ -1,17 +1,11 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from yahooquery import Ticker
 from typing import Dict
+from uuid import uuid4, UUID
 
 
 class Bond(BaseModel):
     principal: float
     interest: float
-    period: str  # enum
-
-    ticker: Optional[str] = None
-    yquery: Dict = Ticker(ticker)
-
-    @property
-    def currency(self):
-        return Ticker(self.ticker).summary_detail.get('AAPL').get('currency')
+    id_: UUID = Field(default_factory=uuid4)
