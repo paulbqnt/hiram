5a000199f18194affecb5bef3b71bdc50335371c
paulbqnt <paul.boquant@gmail.com>
2024-12-04 22:50:28 +0100

    Set up project

---
+++ backend/src/main/kotlin/com/example/hiram/HiramApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hiram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HiramApplication
+
+fun main(args: Array<String>) {
+	runApplication<HiramApplication>(*args)
+}
